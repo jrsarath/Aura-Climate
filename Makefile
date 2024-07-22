2c079ff94fb19f87663a79ea6c524cb564113028
jrsarath <jrsarath@outlook.com>
2024-07-22 15:06:45 +0530

    cleanup

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 #V := 1
-PROJECT_NAME := firmware
+PROJECT_NAME := climate
 EXTRA_COMPONENT_DIRS := $(EXTRA_COMPONENT_DIRS)
 include $(IDF_PATH)/make/project.mk
--- main/globals.cpp
+++ main/globals.cpp
@@ -2,8 +2,12 @@
 
 sgp40_t sgp;
 
+// Sensor values
 float temperature = 0.0;
 float humidity = 0.0;
 int32_t voc_index = 0;
 
-
+// Endpoints
+uint16_t temperature_endpoint_id = 1;
+uint16_t humidity_endpoint_id = 2;
+uint16_t voc_endpoint_id = 3;
