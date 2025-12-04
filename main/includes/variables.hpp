d3b9f8e1513f441cc4170c6bc9511109102005ad
Sarath "Delta" Singh <jrsarath@outlook.com>
2025-12-05 00:19:12 +0530

    rebase

--- main/includes/variables.hpp
+++ main/includes/variables.hpp
@@ -1,11 +1,5 @@
 #pragma once
 
-#include <inttypes.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 // Matter endpoint IDs
 extern uint16_t temperature_endpoint_id;
 extern uint16_t humidity_endpoint_id;
@@ -14,7 +8,3 @@ extern uint16_t voc_endpoint_id;
 // SGP40 sensor timing
 extern const uint32_t SGP40_WARMUP_TIME_MS;  // Warmup time in milliseconds
 extern uint32_t sgp40_start_time_ms;         // Start time timestamp
-
-#ifdef __cplusplus
-}
-#endif
